31a95506db4d980dbae8cee90aafa9b4282cf49e
ravenusmc <mcuddy77@gmail.com>
2018-05-11 16:36:16 -0400

    working on adding a controller

---
+++ controller/categories.php
@@ -0,0 +1,8 @@
+<?php include '../view/header.php'; ?>
+<link rel="stylesheet" type="text/css" href="../assets/css/category.css">
+
+<header>
+  <h1>Categories</h1>
+  <h3>Please Select from the Below</h3>
+</header>
+
--- controller/index.php
+++ controller/index.php
@@ -1,1 +1,22 @@
-index.php
+<?php
+  //This file is the controller for the inventory project 
+  
+  //Setting a default action 
+  $action = filter_input(INPUT_POST, 'action');
+  if ($action == NULL) {
+      $action = filter_input(INPUT_GET, 'action');
+      if ($action == NULL) {
+          $action = 'category_page';
+      }
+  }
+
+  //Switch statment to determine which page to go to. 
+  switch ($action) {
+
+    case 'category_page':
+      include('categories.php');
+      break;
+  }
+
+
+?>
