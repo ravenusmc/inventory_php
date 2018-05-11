f92f9c0cffb0ba18f4ca97fbe50af2736ce03730
ravenusmc <mcuddy77@gmail.com>
2018-05-11 16:12:46 -0400

    added a basic landing page

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php include 'view/header.php'; ?>
+
+<!-- have to include this link to get the css to apply to this file -->
+<link rel="stylesheet" type="text/css" href="./assets/css/landing.css">
+<link rel="stylesheet" type="text/css" href="./assets/css/generic.css">
+
+
+<!-- Start of Header Section -->
+<header>
+  <h1>Inventory System</h1>
+</header>
+<!-- End of Header Section -->
+
+<!-- Start of Main Section -->
+<main>
+  <h1>Enter</h1>
+</main>
+<!-- End of main Section -->
+
+
+
+
+<?php include 'view/footer.php'; ?>
---
+++ view/footer.php
@@ -0,0 +1,7 @@
+  <footer>
+      <p class="font copyright">
+          &copy; <?php echo date("Y"); ?> Cuddy Prison System, Inc.
+      </p>
+  </footer>
+</body>
+</html>
