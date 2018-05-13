f0f373c3ba738ab4b5e33ab5188d04f3bd722688
ravenusmc <mcuddy77@gmail.com>
2018-05-13 13:34:05 -0400

    working on the books section of the project

---
+++ books_controller/books.php
@@ -0,0 +1,16 @@
+<?php include '../view/header.php'; ?>
+<link rel="stylesheet" type="text/css" href="../assets/css/books.css">
+
+<header>
+  <h1>Books Page</h1>
+</header>
+
+<main>
+
+  <h1><a href="?action=add_book">Add Book</a></h1>
+  <h1><a href="?action=search_books">Search for Books</a></h1>
+
+</main>
+
+
+<?php include '../view/footer.php'; ?>
--- books_controller/index.php
+++ books_controller/index.php
@@ -1,18 +1,34 @@
 <?php 
+  //This file is for the books section
 
   // //Pulling in the databases
   // require('../model/database.php');
 
   // $db = Database::getDB();
+   
+  //Setting a default action 
+  $action = filter_input(INPUT_POST, 'action');
+  if ($action == NULL) {
+      $action = filter_input(INPUT_GET, 'action');
+      if ($action == NULL) {
+          $action = 'books';
+      }
+  }
+
+  //Switch statment to determine which page to go to. 
+  switch ($action) {
+
+    case 'books':
+      include('books.php');
+      break;
+    case 'add_book':
+      include('add_book.php');
+      break;
+    case 'search_books':
+      include('search_books.php');
+      break;
+  }
 
 ?>
 
-<?php include '../view/header.php'; ?>
-<link rel="stylesheet" type="text/css" href="../assets/css/books.css">
 
-<header>
-  <
-</header>
-
-
-<?php include '../view/footer.php'; ?>
