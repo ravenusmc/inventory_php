29cc0d2b472b30322c84ce042ef2890d4d6c8d6b
ravenusmc <mcuddy77@gmail.com>
2018-05-19 15:24:09 -0400

    added some styling

--- books_controller/books.php
+++ books_controller/books.php
@@ -1,15 +1,18 @@
 <?php include '../view/header.php'; ?>
+<link rel="stylesheet" type="text/css" href="../assets/css/generic.css">
 <link rel="stylesheet" type="text/css" href="../assets/css/books.css">
-
-<header>
-  <h1>Books Page</h1>
-  <h3>What Would you Like to Do</h3>
-</header>
 
 <main>
 
-  <h1><a href="?action=add_book">Add Book</a></h1>
-  <h1><a href="?action=search_books">Search for Books</a></h1>
+  <div>
+    <h1>Books Page</h1>
+    <h3>What Would you Like to Do</h3>
+  </div>
+
+  <div>
+    <h1><a href="?action=add_book">Add Book</a></h1>
+    <h1><a href="?action=search_books">Search for Books</a></h1>
+  </div>
 
 </main>
 
