cd16953cd34d7d468b920f7c0a4d31cb0bbfaaad
ravenusmc <mcuddy77@gmail.com>
2018-05-19 15:37:46 -0400

    fixed styling on add book page

--- books_controller/add_book.php
+++ books_controller/add_book.php
@@ -1,14 +1,13 @@
 <?php include '../view/header.php'; ?>
+<link rel="stylesheet" type="text/css" href="../assets/css/generic.css">
 <link rel="stylesheet" type="text/css" href="../assets/css/books.css">
-
-<!-- header area -->
-<header>
-  <h1>Add Book</h1>
-</header>
-<!-- End of header area -->
 
 <!-- Start of Main Section -->
 <main>
+
+  <div>
+    <h1>Add Book</h1>
+  </div>
 
   <!-- This form will allow the user to add a book -->
   <form action="index.php" method="post">
@@ -16,14 +15,14 @@
     <input type="hidden" name="action" value="add_book_post" />
 
     <!-- The below inputs will deal with getting information for the book -->
-    <input placeholder='Title' type='text' name='title'><br>
-    <input placeholder='Author' type='text' name='author'><br>
-    <input placeholder='subject' type='text' name='subject'><br>
-    <input placeholder='year' type='text' name='year'><br>
+    <input placeholder='Title' type='text' name='title' required><br>
+    <input placeholder='Author' type='text' name='author' required><br>
+    <input placeholder='subject' type='text' name='subject' required><br>
+    <input placeholder='year' type='text' name='year' required><br>
     <input type='radio' name='category' value='fiction'>Fiction<br>
     <input type='radio' name='category' value='non-fiction'>NonFiction<br>
 
-    <input type="submit" value="Add Book" />
+    <input class='button' type="submit" value="Add Book" />
 
   </form>
   <!-- End of form element -->
