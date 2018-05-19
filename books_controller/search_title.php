c6a1ecf1c8c976d579d6f6f645a180a8fafac1cd
ravenusmc <mcuddy77@gmail.com>
2018-05-19 15:44:49 -0400

    added styling to search by title page

--- books_controller/search_title.php
+++ books_controller/search_title.php
@@ -1,18 +1,25 @@
 <?php include '../view/header.php'; ?>
+<link rel="stylesheet" type="text/css" href="../assets/css/generic.css">
 <link rel="stylesheet" type="text/css" href="../assets/css/books.css">
 
+<!-- Start of main area -->
+<main>
 
-<section>
-  <h3>Please Enter Title of the Book To Search For</h3>
+  <div>  
+    <h3>Please Enter Title of the Book To Search For</h3>
+  </div>
+
   <form action="index.php" method="post">
 
     <input type="hidden" name="action" value="title_search" />
 
-    <input placeholder='Title' type='text' name='title'><br>
-    <input type="submit" value="Search By Title" />
+    <input placeholder='Title' type='text' name='title' required><br>
+    <input class='button' type="submit" value="Search By Title" />
 
   </form>
-</section>
+
+</main>
+<!-- End of main area -->
 
 
 <?php include '../view/footer.php'; ?>
