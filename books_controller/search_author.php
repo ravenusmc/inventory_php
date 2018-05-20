f3d82fa9125592e61e2906d0b2f33366a25300ba
ravenusmc <mcuddy77@gmail.com>
2018-05-20 16:14:32 -0400

    added styling to author page

--- books_controller/search_author.php
+++ books_controller/search_author.php
@@ -1,18 +1,22 @@
 <?php include '../view/header.php'; ?>
 <link rel="stylesheet" type="text/css" href="../assets/css/books.css">
 
+<!-- Start of the main area --> 
+<main>
 
-<section>
-  <h3>Please Enter Author To Search For</h3>
+  <div>
+    <h3>Please Enter Author To Search For</h3>
+  </div>
+
   <form action="index.php" method="post">
 
     <input type="hidden" name="action" value="author_search" />
 
     <input placeholder='Author' type='text' name='author'><br>
-    <input type="submit" value="Search By Author" />
+    <input class='button' type="submit" value="Search By Author" />
 
   </form>
-</section>
-
+</main>
+<!-- End of the main area -->
 
 <?php include '../view/footer.php'; ?>
