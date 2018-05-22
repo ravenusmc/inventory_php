046815b4d2da5a7a83940de0333722ea41fa6095
ravenusmc <mcuddy77@gmail.com>
2018-05-22 15:21:33 -0400

    added in movies controller

--- movies_controller/index.php
+++ movies_controller/index.php
@@ -1,5 +1,31 @@
-<?php 
+<?php
+  //This file will hold all of the code for the movies page
 
- echo 'Movies';
+  //Pulling in the databases
+  require('../model/database.php');
+  require('../model/movie.php');
+  require('../model/movie_db.php');
+
+  //Creating the object to deal with the database. 
+  $MovieDB = new movieDB();
+
+  //Setting a default action 
+  $action = filter_input(INPUT_POST, 'action');
+  if ($action == NULL) {
+      $action = filter_input(INPUT_GET, 'action');
+      if ($action == NULL) {
+          $action = 'movies';
+      }
+  }
+
+  //Switch statment to determine which page to go to. 
+  switch ($action) {
+    //This action will bring the user to the books home page
+    case 'movies':
+      include('movies.php');
+      break;
+  }
+
+
 
 ?>
