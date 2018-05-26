d182fa5d3f5419bd5bc258cb234b92caa065338a
ravenusmc <mcuddy77@gmail.com>
2018-05-26 19:31:45 -0400

    wrote code to get back movie results using ajax

--- movies_controller/getMovieTitle.php
+++ movies_controller/getMovieTitle.php
@@ -7,17 +7,36 @@
 <?php
   
   //Pulling in the databases
-  require('../model/database.php');
-  require('../model/movie.php');
-  require('../model/movie_db.php');
+  // require('../model/database.php');
+  // require('../model/movie.php');
+  // require('../model/movie_db.php');
 
   //Creating the object to deal with the database. 
-  $MovieDB = new movieDB();
-  
-  $q = $_GET['q'];
+  //$MovieDB = new movieDB();
 
-  echo $q;
+  //Searching for the movie in the database by title
+  //$movie = $MovieDB->searchByTitle($title);
+  // echo $movie->getTitle();
   
+  $title = $_GET['q'];
+
+  $con = mysqli_connect('localhost','root','root','inventory');
+  if (!$con) {
+      die('Could not connect: ' . mysqli_error($con));
+  }
+
+  mysqli_select_db($con,"ajax_demo");
+  $sql="SELECT * FROM movies WHERE title = '".$title."'";
+  $result = mysqli_query($con,$sql);
+
+  while($row = mysqli_fetch_array($result)) {
+    echo "<td>" . $row['genre'] . "</td>";
+    echo "<td>" . $row['director'] . "</td>";
+  }
+
+  mysqli_close($con);
+
+
 
 ?>
 
