21c427d9ed0edf07fbf6671e868758b4aa23f68d
ravenusmc <mcuddy77@gmail.com>
2018-06-02 19:05:22 -0400

    adding comments

--- movies_controller/search_director.php
+++ movies_controller/search_director.php
@@ -1,15 +1,19 @@
 <?php include '../view/header.php';  ?>
 
+<!-- Start of main area -->
 <main>
 
+<!--heading area -->
 <h1 class='director_heading'>Search for Movie By Director</h1>
 
+<!-- form area -->
 <form>
   <input id='director' placeholder='Director'><br>
   <div>
     <input onclick='getMovieByDirector(); return false;' class='button' type="submit" value="Search By Director" />
   </div>
 </form>
+<!-- End of form area -->
 
 <br>
 
@@ -19,6 +23,7 @@
 </div>
 
 </main>
+<!-- End of main area -->
 
 
 <script type="text/javascript" src='../assets/js/index.js'></script>
