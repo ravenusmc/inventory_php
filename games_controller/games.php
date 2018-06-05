0ed88b46aceb61b308752dc3171fc2d774af323c
ravenusmc <mcuddy77@gmail.com>
2018-06-05 14:26:48 -0400

    added to the games controller and added a games page

---
+++ games_controller/games.php
@@ -0,0 +1,20 @@
+<?php include '../view/header.php'; ?>
+
+<!-- Start of main section -->
+<main>
+
+  <div>
+    <h1>Games Page</h1>
+    <h3>What Would you Like to Do</h3>
+  </div>
+
+  <div>
+    <h1><a href="?action=add_movie">Add Game</a></h1>
+    <h1><a href="?action=search_movies">Search for Games</a></h1>
+  </div>
+
+</main>
+<!-- End of main Section -->
+
+
+<?php include '../view/footer.php'; ?>
--- games_controller/index.php
+++ games_controller/index.php
@@ -1,5 +1,31 @@
 <?php 
+  //This file will hold all of the code for the movies page
 
-echo 'Games';
+  //Pulling in the databases
+  // require('../model/database.php');
+  // require('../model/games.php');
+  // require('../model/game_db.php');
+
+  //Creating the object to deal with the database. 
+  //$GameDB = new gameDB();
+
+  //Setting a default action 
+  $action = filter_input(INPUT_POST, 'action');
+  if ($action == NULL) {
+      $action = filter_input(INPUT_GET, 'action');
+      if ($action == NULL) {
+          $action = 'games';
+      }
+  }
+
+  //Switch statment to determine which page to go to. 
+  switch ($action) {
+
+    //This action will bring the user to the games home page
+    case 'games':
+      include('games.php');
+      break;
+  }
+
 
 ?>
