21118225f999e81f00df2b86126489c78c5352e7
ravenusmc <mcuddy77@gmail.com>
2018-06-08 15:13:44 -0400

    added game class

--- model/game.php
+++ model/game.php
@@ -2,7 +2,56 @@
   
   class Game {
 
-    
+    private $id, $title, $rating, $genre, $year;
+
+    public function __construct() {
+      $this->id = 0;
+      $this->title = '';
+      $this->rating = 0;
+      $this->genre = '';
+      $this->year = 0;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function setId($value) {
+        $this->id = $value;
+    }
+
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function setTitle($value) {
+        $this->title = $value;
+    }
+
+    public function getRating() {
+        return $this->rating;
+    }
+
+    public function setDirector($value) {
+        $this->rating = $value;
+    }
+
+        public function getGenre() {
+        return $this->genre;
+    }
+
+    public function setGenre($value) {
+        $this->genre = $value;
+    }
+
+
+    public function getYear() {
+        return $this->year;
+    }
+
+    public function setYear($value) {
+        $this->year = $value;
+    }
 
   }
 
